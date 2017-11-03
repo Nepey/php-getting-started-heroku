cefa3570bb53cb11d3c77be59f1890f209d46667
Nepey <31688202+Nepey@users.noreply.github.com>
2017-11-03 19:50:57 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,54 +1,249 @@
 <!DOCTYPE html>
-<html lang="en" >
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Form test</title>
-</head>
-<body>
-Life is life
-  <?php if (isset($_GET['submitted'])) { ?>
-    <h1>The server had accepted:</h1>
-    <p>radio: <?php echo htmlspecialchars($_GET['radio']); ?></p>
-    <p>Try again? <a href="/index.php">Yes!</a></p>
-  <?php } ?>
-
-  <?php if (!isset($_GET['submitted'])) { ?>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="http://bootstrap-3.ru/assets/ico/favicon.ico">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>ДОБРАЯ ДОРОГА</title>
+    <!-- Bootstrap core CSS -->
+    <link href="./Carousel Template for Bootstrap_files/bootstrap.css" rel="stylesheet">
+    <link href="./Carousel Template for Bootstrap_files/good_road.css" rel="stylesheet">
+          
+     <style id="holderjs-style" type="text/css"></style>
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+   <div class="navbar-wrapper">
+      <div class="container">
+        <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Открыть навигацию</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\Introduction.html"><img src="D:\LearnVSCode\GOOD ROAD\GooD RoaD1\method-draw-image (2).svg" class="img-responsive" alt="Blockchain"></a>
+            </div>
+            <div class="collapse navbar-collapse" id="responsive menu">
+              <ul class="nav navbar-nav">
+                     <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Концепция ДОБРАЯ ДОРОГА<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                           <li><a href="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\general measures.html"> Общие меры</a></li>
+                            <li class="divider"></li>
+                            <li><a href="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\direct measures.html">Прямые меры</a></li>
+                             <li class="divider"></li>
+                             <li><a href="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\structure_program.html">Структурные программы</a></li> 
+                        </ul>
+                     <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Комиссия по обеспечению безопасности дорожного движения при Мингорисполкоме<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                           <li><a href="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\About_comission.html">Положение о комиссии</a></li>
+                            <li class="divider"></li>
+                           <li><a href="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\Structure_comission.html">Схема работы комисии</a></li>
+                            <li class="divider"></li>
+                           <li><a href="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\Carousel Template for Bootstrap_files\протоколы комиссий.rar">Решения комиссии</a></li>
+                        </ul>
+                     </li>
+                 </ul>
+              </div> 
+         </div>
+       <!-- Carousel
+    ================================================== -->
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\Carousel Template for Bootstrap_files\razvyazka_dvizhenie_nochnoj_gorod_vid_sverkhu_116498_1920x1080.jpg" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Engineering (Инфраструктура)</h1>
+              <p>Дизайнер дороги закладывает в нее все последствия</p>
+              <p><a class="btn btn-lg btn-primary" href="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\Engineering.html" role="button">дизайн транспортной инфраструктуры</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\Carousel Template for Bootstrap_files\oboitut.com_1420448366.jpg" alt = "">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Education (Образование)</h1>
+              <p>Общество должно быть информировано и обучено безопасному поведению на дороге </p>
+              <p><a class="btn btn-lg btn-primary" href="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\Education.html" role="button">образование</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\Carousel Template for Bootstrap_files\Safetyininformationandtrafficsystems_teaser.jpg" alt=""> 
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Enforcement (Контроль)</h1>
+              <p>Контроль должен быть разумным и эффективным</p>
+              <p><a class="btn btn-lg btn-primary" href="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\Control.html" role="button">Контроль</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
   
-  <h1>Play with the HTML form</h1>
-  <form action="/index.php">
-    <input type="hidden" name="submitted" value="yes">
-    <p>
-      <label for="my-radio-yes">radio Yes: </label>
-      <input id="my-radio-yes" name="radio" type="radio" required value="Yes">
-      <label for="my-radio-no">radio No: </label>
-      <input id="my-radio-no" name="radio" type="radio" required value="No"></p>
-    <br>
-   
-  
-    <input type="hidden" name="submitted" value="yes">
-    <input type="text" name="text" required maxlength="6" placeholder="введите 6 букв"><br>
-    <input type="color" name="color"></p>
-      <p><input type='date' name="date"></p>
-      <p><input type='month' name="month"></p>
-      <p><input type="email" name="email"></p>
-      <p><input type="number" name="number"></p>
-      <p><input type="range" name="range"></p>
-      <p><input type="checkbox" name="checkbox"></p>
-      <p><select id="my-select" name="myselect" size="1" required>
-   <option value="">---------</option>
-   <option value="1">любит</option>
-   <option value="2">не любит</option>
-      </select></p>
-      <textarea id="my-textarea" style="resize:none" maxlength="10" placeholder="введите аккуратно"></textarea>
-      
-    <br>
-    <input type="submit" value="Submit">
-    <input type="reset" value="Clear">
-  </form>
+      <!-- Left and right controls -->
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+      </a>
+   </div>
+  </div>  
+</div>
+<!-- Marketing messaging and featurettes
+<! Marketing messaging and featurettes ================================================== -->
+<!-- Wrap the rest of the page in another container to center all the content. -->
+  <div class="container marketing">
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6">
+          <img class="img-circle" src="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\Carousel Template for Bootstrap_files\doroga_zakat_razmetki_trava_102104_1920x1080.jpg" alt="140x140" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNDAiIGhlaWdodD0iMTQwIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjcwIiB5PSI3MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjE0MHgxNDA8L3RleHQ+PC9zdmc+" style="width: 300px; height: 200px;">
+          <h2>"Добрая дорога" 2011-2015г.г.</h2>
+          <p><a class="btn btn-default" href="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\Carousel Template for Bootstrap_files\результаты Добрая дорога 2011-2015.pdf" role="button">Просмотреть  результаты »</a></p>
+        </div><!-- /col-lg-6 col-md-6 col-sm-6 -->
+        <div class="col-lg-6 col-md-6 col-sm-6">
+          <img class="img-circle" src="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\Carousel Template for Bootstrap_files\doroga_gory_razmetki_93576_1920x1080.jpg" alt="140x140" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNDAiIGhlaWdodD0iMTQwIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjcwIiB5PSI3MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjE0MHgxNDA8L3RleHQ+PC9zdmc+" style="width: 300px; height: 200px;">
+          <h2>"Добрая дорога" 2016-2020г.г.</h2>
+          <p><a class="btn btn-default" href="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\Carousel Template for Bootstrap_files\Добрая дорога 2016-2020.gif" role="button">Просмотреть результаты »</a></p>
+        </div><!-- /.col-lg-6 col-md-6 col-sm-6 -->
+      </div><!-- /.row -->
 
-  <?php } ?>
 
-</body>
+      <!-- START THE FEATURETTES -->
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h3 class="featurette-heading">Незащищенные участники дорожного движения <br><span class="text-muted">дети, пожилые люди, велосипедисты, инвалиды</span></h3>
+          <p class="lead"><a href="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\Carousel Template for Bootstrap_files\Незащищенные участники дорожного движения.pdf">Дорожная инфраструктура в городской среде должна в первую очередь учитывать интересы незащищенных участников дорожного движения.</a></p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive" src="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\Carousel Template for Bootstrap_files\1.freedomtomove_teaser.jpg" alt="700x700" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI1MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjI1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojYWFhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjMxcHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+NTAweDUwMDwvdGV4dD48L3N2Zz4=">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive" src="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\Carousel Template for Bootstrap_files\rabstol_net_china_21_2560x1440.jpg" alt="500x500" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI1MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjI1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojYWFhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjMxcHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+NTAweDUwMDwvdGV4dD48L3N2Zz4=">
+        </div>
+        <div class="col-md-7">
+          <h3 class="featurette-heading">Структуризация<br><span class="text-muted">улично-дорожной сети</span></h3>
+          <p class="lead"><a href="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\Structurization.html">Улично-дорожная сеть должна быть функционально разделена на зоны с различной допустимой скоростью движения:30км/час,50км/час,70км/час. 
+            Скорость движения транспортных средств в каждой зоне должна быть обеспечена за счет проектных решений.</a></p>
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h3 class="featurette-heading">Интеллектуальные технологии<br><span class="text-muted">в транспортных системах</span></h3>
+          <p class="lead"><a href="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\ITS Minsk.html">Новые интеллектуальные технологии призваны повысить безопасность и комфорт участников дорожного движения. 
+            ITS (Интеллектуальная транспортная система), Smart City ("Умный город"), Safety City("Безопасный город"), UTCS (Автоматизированная система управления дорожным движением), автономные автомобили
+             - это реальность, переходящая в будущее.</a></p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive" src="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\Carousel Template for Bootstrap_files\ITS.png" alt="500x500" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI1MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjI1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojYWFhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjMxcHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+NTAweDUwMDwvdGV4dD48L3N2Zz4=">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <!-- /END THE FEATURETTES -->
+      <!--FORM-->
+     <div class="form"> 
+      <div class="container">
+         <div class="row">
+          <div class="col-lg-8 col-lg-offset-2">
+            <h4 class="text-center">В случае если у Вас есть вопросы или пожелания - зарегистрируйтесь и оставьте свое сообщение </h4>
+            <form id="contact-form" method="post" action="contact.php" role="form">
+             <div class="messages"></div>
+             <div class="controls">
+               <div class="row">
+                 <div class="col-md-6">
+                   <div class="form-group">
+                      <label for="form_name">Ваша фамилия</label>
+                      <input id="form_name" type="text" name="name" class="form-control" placeholder="Пожалуйста, введите Вашу фамилию" required="required" data-error="Firstname is required.">
+                      <div class="help-block with-errors"></div>
+                   </div>
+                 </div>
+                 <div class="col-md-6">
+                    <div class="form-group">
+                       <label for="form_lastname">Ваше имя</label>
+                       <input id="form_lastname" type="text" name="surname" class="form-control" placeholder="Пожалуйста, введите Ваше имя" required="required" data-error="Lastname is required.">
+                       <div class="help-block with-errors"></div>
+                    </div>
+                 </div>
+               </div>
+               <div class="row">
+                  <div class="col-md-6">
+                     <div class="form-group">
+                       <label for="form_email">e-mail: адрес</label>
+                       <input id="form_email" type="email" name="email" class="form-control" placeholder="Пожалуйства введите Ваш e-mail" required="required" data-error="Valid email is required.">
+                       <div class="help-block with-errors"></div>
+                     </div>
+                   </div>
+                 <div class="col-md-6">
+                    <div class="form-group">
+                     <label for="form_phone">Номер телефона</label>
+                     <input id="form_phone" type="tel" name="phone" class="form-control" placeholder="Пожалуйста, введите Ваш номер телефона">
+                     <div class="help-block with-errors"></div>
+                   </div>
+                 </div>
+               </div>
+               <div class="row">
+                  <div class="col-md-12">
+                    <div class="form-group">
+                        <label for="form_message">Текст сообщения</label>
+                        <textarea id="form_message" name="message" class="form-control" placeholder="Текст сообщения" rows="4" required="required" data-error="Пожалуйста, введите текст сообщения"></textarea>
+                        <div class="help-block with-errors"></div>
+                    </div>
+                  </div>
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <div class="g-recaptcha" data-sitekey="6LfKURIUAAAAAO50vlwWZkyK_G2ywqE52NU7YO0S"></div>
+                    </div>
+                  </div>
+                  <div class="col-md-12">
+                      <input type="submit" class="btn btn-success btn-send" value="Отправить сообщение">
+                  </div>
+               </div>
+            </div>
+          </form>
+        </div><!-- /.col-lg-8 col-lg-offset-2 -->
+      </div> <!-- /.row-->
+    </div> <!-- /.container-->
+    <hr class="featurette-divider">
+    
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="D:\LearnVSCode\GOOD ROAD\САРУСЕЛЬ\GOOD ROAD+.html">Наверх</a></p>
+        <p>Контакты: г.Минск,пр.Дзержинского, 3, <br> тел.+375(017)2946176, факс +375(017)2945581</p>
+      </footer>
+   </div>  <!-- /.form-->
+  </div><!-- /.container marketing -->
+  </body>
 </html>
